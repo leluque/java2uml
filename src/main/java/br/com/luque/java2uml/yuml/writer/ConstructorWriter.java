3c89f31c652a8538e70f10a7feb4f89d733fc508
Leandro Luque <leandro.luque@gmail.com>
2023-11-10 15:55:01 -0300

    feat: add << and >> to constructor stereotype

--- src/main/java/br/com/luque/java2uml/yuml/writer/ConstructorWriter.java
+++ src/main/java/br/com/luque/java2uml/yuml/writer/ConstructorWriter.java
@@ -12,7 +12,7 @@ public class ConstructorWriter {
 
 		String result = YUMLHelper
 				.getVisibilityText(method.getVisibility());
-		result += "$$create$$";
+		result += "\u2AA1create\u2AA2";
 		result += method.getName();
 		result += "(";
 		result += Stream.of(method.getParameters()).map(p -> String.format("%s:%s", p.getName(), p.getType().getName())).collect(Collectors.joining(","));

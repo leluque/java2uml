b3b4d369e233a22f1394ba4745ec6eec3f24379f
Leandro Luque <leandro.luque@gmail.com>
2023-11-13 09:59:08 -0300

    fix: fix issue with relationships with arrays of arrays

--- src/main/java/br/com/luque/java2uml/reflection/model/RelationshipField.java
+++ src/main/java/br/com/luque/java2uml/reflection/model/RelationshipField.java
@@ -5,8 +5,11 @@ import br.com.luque.java2uml.reflection.annotation.Aggregation;
 import br.com.luque.java2uml.reflection.annotation.Composition;
 import br.com.luque.java2uml.reflection.annotation.MappedBy;
 
+import java.lang.reflect.GenericArrayType;
 import java.lang.reflect.ParameterizedType;
 import java.lang.reflect.Type;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 @SuppressWarnings("unused")
@@ -71,54 +74,77 @@ public class RelationshipField extends Field {
         if (isOneRelationship(field, clazzPool)) {
             return true;
         }
-        if (isArrayRelationship(field, clazzPool)) {
+        if (isPureArrayRelationship(field, clazzPool)) {
             return true;
         }
-        return isCollectionRelationship(field, clazzPool);
+        return isGenericRelationship(field, clazzPool);
     }
 
     private static boolean isOneRelationship(java.lang.reflect.Field field, ClazzPool clazzPool) {
         return clazzPool.getRules().includes(field.getType());
     }
 
-    private static boolean isArrayRelationship(java.lang.reflect.Field field, ClazzPool clazzPool) {
-        return field.getType().isArray()
-            && clazzPool.getRules().includes(field.getType().getComponentType());
+    private static boolean isPureArrayRelationship(java.lang.reflect.Field field, ClazzPool clazzPool) {
+        if (!field.getType().isArray()) {
+            return false;
+        }
+
+        // Handle the case of arrays of arrays ...
+        Class<?> componentType = field.getType().getComponentType();
+        while (componentType.isArray()) {
+            componentType = componentType.getComponentType();
+        }
+        return clazzPool.getRules().includes(componentType);
     }
 
     private static boolean isCollectionRelationship(java.lang.reflect.Field field, ClazzPool clazzPool) {
+        return field.getType().getPackageName().startsWith("java.util");
+    }
+
+    private static boolean isGenericRelationship(java.lang.reflect.Field field, ClazzPool clazzPool) {
         if (!field.getType().getPackageName().startsWith("java.util")) {
             return false;
         }
-        ParameterizedType generics = (ParameterizedType) field.getGenericType();
-        Type[] typeArguments = generics.getActualTypeArguments();
-        for (Type typeArgument : typeArguments) {
-            if (typeArgument instanceof Class<?> originalClass
-                && clazzPool.getRules().includes(originalClass)) {
-                return true;
-            }
-        }
-        return false;
+
+        return extractScopedGenerics(field, clazzPool).length > 0;
     }
 
-    private void extractRelationshipInfo() {
-        if (isOneRelationship(getField(), getClazzPool())) {
-            cardinality = Cardinalities.ONE;
-            otherSide = getClazzPool().getFor(getField().getType());
-        } else if (isArrayRelationship(getField(), getClazzPool())) {
-            cardinality = Cardinalities.N;
-            otherSide = getClazzPool().getFor(getField().getType().getComponentType());
-        } else if (isCollectionRelationship(getField(), getClazzPool())) {
-            cardinality = Cardinalities.N;
-            ParameterizedType generics = (ParameterizedType) getField().getGenericType();
+    private static Class<?>[] extractScopedGenerics(java.lang.reflect.Field field, ClazzPool clazzPool) {
+        // Handle the case of arrays of arrays of collections ...
+        Type genericFieldType = field.getGenericType();
+        while (genericFieldType instanceof GenericArrayType) {
+            genericFieldType = ((GenericArrayType) genericFieldType).getGenericComponentType();
+        }
+
+        List<Class<?>> scopedGenerics = new ArrayList<>();
+        if (genericFieldType instanceof ParameterizedType) {
+            ParameterizedType generics = (ParameterizedType) genericFieldType;
             Type[] typeArguments = generics.getActualTypeArguments();
             for (Type typeArgument : typeArguments) {
                 if (typeArgument instanceof Class<?> originalClass
-                    && getClazzPool().getRules().includes(originalClass)) {
-                    otherSide = getClazzPool().getFor(originalClass);
-                    break;
+                    && clazzPool.getRules().includes(originalClass)) {
+                    scopedGenerics.add(originalClass);
                 }
             }
+        }
+        return scopedGenerics.toArray(new Class<?>[0]);
+    }
+
+    private void extractRelationshipInfo() {
+        if (isPureArrayRelationship(getField(), getClazzPool())) {
+            cardinality = Cardinalities.N;
+            otherSide = getClazzPool().getFor(getField().getType().getComponentType());
+        } else if (isGenericRelationship(getField(), getClazzPool())) {
+            cardinality = isCollectionRelationship(getField(), getClazzPool()) ? Cardinalities.N : Cardinalities.ONE;
+
+            // TODO: handle multiple relationships (more than one generic in the scope).
+            Class<?>[] scopedGenerics = extractScopedGenerics(getField(), getClazzPool());
+            if (scopedGenerics.length > 0) {
+                otherSide = getClazzPool().getFor(scopedGenerics[0]);
+            }
+        } else if (isOneRelationship(getField(), getClazzPool())) { // This one must go last because it doesn't check if it is array nor collection to avoid double check.
+            cardinality = Cardinalities.ONE;
+            otherSide = getClazzPool().getFor(getField().getType());
         }
 
         MappedBy mappedByAnnotation = getField().getAnnotation(MappedBy.class);

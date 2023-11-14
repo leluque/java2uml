a2f679699ffa04e7109f060c8ef09cda5c88194e
Leandro Luque <leandro.luque@gmail.com>
2023-11-14 10:11:47 -0300

    fix: fix issue with return type void

--- src/main/java/br/com/luque/java2uml/yuml/writer/classdiagram/YUMLMethodWriter.java
+++ src/main/java/br/com/luque/java2uml/yuml/writer/classdiagram/YUMLMethodWriter.java
@@ -12,6 +12,6 @@ public class YUMLMethodWriter implements MethodWriter {
         Objects.requireNonNull(method);
 
         return YUMLHelper
-            .getVisibilityText(method.getVisibility()) + method.getName() + "(" + Stream.of(method.getParameters()).map(p -> String.format("%s:%s", p.getName(), p.getType().getName())).collect(Collectors.joining(",")) + "):" + method.getReturnType().getName();
+            .getVisibilityText(method.getVisibility()) + method.getName() + "(" + Stream.of(method.getParameters()).map(p -> String.format("%s:%s", p.getName(), p.getType().getName())).collect(Collectors.joining(",")) + "):" + (method.getReturnType() == null ? "void" : method.getReturnType().getName());
     }
 }

45d8c7c129f55effedc6424603af8765e36856f8
Leandro Luque <leandro.luque@gmail.com>
2023-11-14 10:19:19 -0300

    test: add tests to YUMLHelper

---
+++ src/test/java/br/com/luque/java2uml/yuml/writer/classdiagram/YUMLHelperTest.java
@@ -0,0 +1,27 @@
+package br.com.luque.java2uml.yuml.writer.classdiagram;
+
+import br.com.luque.java2uml.core.reflection.model.Visibilities;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class YUMLHelperTest {
+    /**
+     * {@link YUMLHelper#getVisibilityText(Visibilities)}
+     */
+    @Test
+    void givenVisibility_whenGetYUMLRepresentation_thenProduceCorrectResult() {
+        assertEquals("~", YUMLHelper.getVisibilityText(Visibilities.PACKAGE));
+        assertEquals("+", YUMLHelper.getVisibilityText(Visibilities.PUBLIC));
+        assertEquals("#", YUMLHelper.getVisibilityText(Visibilities.PROTECTED));
+        assertEquals("-", YUMLHelper.getVisibilityText(Visibilities.PRIVATE));
+    }
+
+    /**
+     * {@link YUMLHelper#getStereotypeText(String)}
+     */
+    @Test
+    void givenStereotype_whenGetYUMLRepresentation_thenProduceCorrectResult() {
+        assertEquals("⪡Stereotype⪢", YUMLHelper.getStereotypeText("Stereotype"));
+    }
+}
